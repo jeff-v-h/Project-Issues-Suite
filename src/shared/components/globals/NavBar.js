ca4c32afbc9f21b16196aea0fd9361db874c5ca9
Jeffrey Huang <jeffvh@outlook.com>
2019-10-06 18:38:43 +0100

    update image

--- src/shared/components/globals/NavBar.js
+++ src/shared/components/globals/NavBar.js
@@ -1,16 +1,16 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import { bool, func, instanceOf, object } from 'prop-types';
-import { connect } from 'react-redux';
-import { bindActionCreators } from 'redux';
-import { Menu, Icon, Responsive, Image } from 'semantic-ui-react';
-import { withCookies, Cookies } from 'react-cookie';
-import toastr from 'toastr';
+import React from "react";
+import { Link } from "react-router-dom";
+import { bool, func, instanceOf, object } from "prop-types";
+import { connect } from "react-redux";
+import { bindActionCreators } from "redux";
+import { Menu, Icon, Responsive, Image } from "semantic-ui-react";
+import { withCookies, Cookies } from "react-cookie";
+import toastr from "toastr";
 
-import UserMenu from '../user/UserMenu';
-import { updateUser } from '../../actions/user-actions';
-import { routeCodes } from '../../config/constants';
-import d3Logo from '../../../../public/images/Deltatre_logo_RGB_red_150x30.png';
+import UserMenu from "../user/UserMenu";
+import { updateUser } from "../../actions/user-actions";
+import { routeCodes } from "../../config/constants";
+import logo from "../../../../public/images/project-design-amped-name.png";
 
 const propTypes = {
   isAuthed: bool.isRequired,
@@ -28,32 +28,32 @@ class NavBar extends React.Component {
 
   componentDidMount() {
     this.updateWindowDimensions();
-    window.addEventListener('resize', this.updateWindowDimensions);
+    window.addEventListener("resize", this.updateWindowDimensions);
   }
 
   componentWillUnmount() {
-    window.removeEventListener('resize', this.updateWindowDimensions);
+    window.removeEventListener("resize", this.updateWindowDimensions);
   }
 
   updateWindowDimensions = () => {
     if (window.innerWidth > 767) {
-      this.setState({showNav: true});
+      this.setState({ showNav: true });
     } else {
-      this.setState({showNav: false});
+      this.setState({ showNav: false });
     }
-  }
+  };
 
   changeShowState = () => {
     this.setState(prevState => ({
       show: !prevState.show
     }));
-  }
+  };
 
   changeTheme = () => {
-    const app = document.getElementById('app');
+    const app = document.getElementById("app");
     let theme;
-    const light = 'theme-light';
-    const dark = 'theme-dark';
+    const light = "theme-light";
+    const dark = "theme-dark";
     const { actions, users } = this.props;
 
     if (app.classList.contains(light)) {
@@ -70,47 +70,84 @@ class NavBar extends React.Component {
 
     // change the redux stored user's theme to the selected theme and update the db
     users.data.theme = theme;
-    actions.updateUser(users.data.signinName, users.data)
-      .catch(err => toastr.error('An error occurred trying to update user theme preference'));
-  }
+    actions
+      .updateUser(users.data.signinName, users.data)
+      .catch(err =>
+        toastr.error("An error occurred trying to update user theme preference")
+      );
+  };
 
   showHideNav = () => {
     this.setState(prevState => ({
       showNav: !prevState.showNav
     }));
-  }
+  };
 
   render() {
     return (
       <Menu stackable className="nav-menu-wrapper">
         <Responsive {...Responsive.onlyMobile} className="nav-menu">
-          <Image src={d3Logo} size="small" alt="monograme" floated="left"
-            className="hamburger-menu logo" as={Link} to={routeCodes.DASHBOARD}/>
-          <Icon name="sidebar" size="large" onClick={this.showHideNav}
-            className="float-right hamburger-menu"/>
+          <Image
+            src={logo}
+            size="small"
+            alt="monograme"
+            floated="left"
+            className="hamburger-menu logo"
+            as={Link}
+            to={routeCodes.DASHBOARD}
+          />
+          <Icon
+            name="sidebar"
+            size="large"
+            onClick={this.showHideNav}
+            className="float-right hamburger-menu"
+          />
         </Responsive>
-        {this.state.showNav &&
+        {this.state.showNav && (
           <Menu stackable borderless fluid className="nav-menu">
             <Responsive minWidth={768}>
               <Menu.Item>
-                <Image src={d3Logo} size="small" alt="logo" as={Link} to={routeCodes.DASHBOARD}/>
+                <Image
+                  src={logo}
+                  size="small"
+                  alt="logo"
+                  as={Link}
+                  to={routeCodes.DASHBOARD}
+                />
               </Menu.Item>
             </Responsive>
-            <Menu.Item name="Dashboard" as={Link} to={routeCodes.DASHBOARD} className="nav-item"/>
-            <Menu.Item name="Browse" as={Link} to={routeCodes.BROWSE} className="nav-item"/>
+            <Menu.Item
+              name="Dashboard"
+              as={Link}
+              to={routeCodes.DASHBOARD}
+              className="nav-item"
+            />
+            <Menu.Item
+              name="Browse"
+              as={Link}
+              to={routeCodes.BROWSE}
+              className="nav-item"
+            />
             <Menu.Menu position="right">
-              <Menu.Item name="Contact" as={Link} to={routeCodes.CONTACT} className="nav-item"/>
+              <Menu.Item
+                name="Contact"
+                as={Link}
+                to={routeCodes.CONTACT}
+                className="nav-item"
+              />
               {this.props.isAuthed ? (
-                <UserMenu signOut={this.props.signOut}
+                <UserMenu
+                  signOut={this.props.signOut}
                   displayName={this.props.users.data.displayName}
                   changeTheme={this.changeTheme}
-                  role={this.props.users.data.role} />
+                  role={this.props.users.data.role}
+                />
               ) : (
                 <Menu.Item name="Login" as={Link} to={routeCodes.LOGIN} />
               )}
             </Menu.Menu>
           </Menu>
-        }
+        )}
       </Menu>
     );
   }
@@ -118,12 +155,17 @@ class NavBar extends React.Component {
 
 NavBar.propTypes = propTypes;
 
-const mapStateToProps = (state) => ({
+const mapStateToProps = state => ({
   users: state.users
 });
 
-const mapDispatchToProps = (dispatch) => ({
+const mapDispatchToProps = dispatch => ({
   actions: bindActionCreators({ updateUser }, dispatch)
 });
 
-export default withCookies(connect(mapStateToProps, mapDispatchToProps)(NavBar));
+export default withCookies(
+  connect(
+    mapStateToProps,
+    mapDispatchToProps
+  )(NavBar)
+);
--- webpack.common.config.js
+++ webpack.common.config.js
@@ -1,11 +1,11 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const CleanWebpackPlugin = require('clean-webpack-plugin');
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CleanWebpackPlugin = require("clean-webpack-plugin");
 
 // Makes the script crash on unhandled rejections instead of silently
 // ignoring them. In the future, promise rejections that are not handled will
 // terminate the Node.js process with a non-zero exit code.
-process.on('unhandledRejection', err => {
+process.on("unhandledRejection", err => {
   throw err;
 });
 
@@ -18,21 +18,21 @@ module.exports = {
     // other stuff which is not the starting point of execution.
     // Hence below vendors may need to be refactored.
     // NB: The entry points will help optimization.splitChunks decide how to chunk the bundles
-    app: './src/index.js',
-    vendors: './src/vendors/vendors.js'
+    app: "./src/index.js",
+    vendors: "./src/vendors/vendors.js"
   },
   output: {
-    path: path.resolve(__dirname, 'build'),
-    publicPath: '/'
+    path: path.resolve(__dirname, "build"),
+    publicPath: "/"
   },
   plugins: [
-    new CleanWebpackPlugin(['build']),
+    new CleanWebpackPlugin(["build"]),
     // Generates an `index.html` file with the <script> injected.
     new HtmlWebpackPlugin({
-      title: 'Test Upload Suite',
+      title: "Project Issues Suite",
       inject: true,
-      template: './index.html',
-      filename: './index.html'
+      template: "./index.html",
+      filename: "./index.html"
     })
   ],
   module: {
@@ -41,14 +41,14 @@ module.exports = {
         enforce: "pre",
         test: /\.(js|jsx|mjs)$/,
         exclude: /node_modules/,
-        loader: "eslint-loader",
+        loader: "eslint-loader"
       }
     ]
   },
   resolve: {
     alias: {
       // allows import by calling 'vendors' rather then traversing relative paths: '../../src/vendors'
-      'vendors$': path.resolve(__dirname, 'src/vendors/vendors.js')
+      vendors$: path.resolve(__dirname, "src/vendors/vendors.js")
     }
   }
 };
